61ad8bc40b247ba1f79b0ebf5f738513d0bfaca3
michalsobecki <michal.sobecki@paker.fit>
2017-07-07 15:10:39 +0200

    deployment

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 ssh -v root@46.101.133.26
-echo '1. Updating sources'
 cd /root/javaapp/
 git checkout --force master
 git pull
 mvn package
 java -jar target/demo-0.0.1-SNAPSHOT.jar
-echo 'Done!'
